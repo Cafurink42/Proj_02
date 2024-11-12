6e9cc4453e16898232ceed8dae1c6d1df0303802
Cafurink42 <carlosdanielcotticaguth@gmail.com>
2024-11-11 22:17:56 -0300

    Fazendo leves melhorias na estrutura base para a aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ main() =>
 
 @override //método dizendo para sobreescrever o método build
 class PerguntaApp extends StatelessWidget {
+  // a classe PerguntaApp é a raiz da aplicação e tem como um do seus filhos diretos o MaterialApp
   Widget build(BuildContext context) {
     //método build que recebe um BuildContext
     return MaterialApp(
-      home: Text("Olá Flutter!"),
+      home: Text(
+          "Olá Flutter!"), //o parâmetro home é nomeado enquanto o Text é posicional isso significa também que ele é obrigatório
     );
   }
 }
